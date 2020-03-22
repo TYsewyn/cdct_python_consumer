8a85ad275dc3f74c035f9d29ad6b95fbaf6ffd0b
Andreas Evers <krosan@gmail.com>
2020-03-22 16:10:21 +0100

    Fixed formatting

--- prereqs.sh
+++ prereqs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" == "Darwin" ]; then
-    echo "Mac OS X platform detected"
-    if ! [ -x "$(command -v brew)" ]; then
+  echo "Mac OS X platform detected"
+  if ! [ -x "$(command -v brew)" ]; then
 		echo 'Brew is not installed, trying SDKMAN!'
 		if ! [ -x "$(command -v sdk)" ]; then
 			echo 'SDKMAN! is not installed, exiting.'
@@ -10,7 +10,7 @@ if [ "$(uname)" == "Darwin" ]; then
 			sdk install springboot
 			spring --version
 			spring install org.springframework.cloud:spring-cloud-cli:2.2.2.RELEASE
-		fi
+	  fi
 	else
 		brew tap pivotal/tap
 		brew install springboot
@@ -21,6 +21,6 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "GNU/Linux platform detected"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     echo "32 bits Windows NT platform detected"
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+elif [ "$(exspr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     echo "64 bits Windows NT platform detected"
 fi
